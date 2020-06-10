6412dbe3e067df4b05a4c914f81cd199ed85d83d
Pawel Trojanek <trojanek@me.com>
2020-06-10 10:46:00 +0200

    Fix logo styling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { styled } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Container from '@material-ui/core/Container';
@@ -6,59 +6,68 @@ import Box from '@material-ui/core/Box';
 import Logo from '../svgs/logo.svg';
 import Link from './Link';
 
-const JJLogo = styled(Logo)({
-  width: 120,
-});
-
 interface LayoutProps {
   children: any
 }
 
-const Layout = ({ children }: LayoutProps) => (
-  <>
-    <AppBar color="inherit">
-      <Toolbar>
-        <Box
-          display="flex"
-          flex="1 1 100%"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Link href="/">
-            <JJLogo />
-          </Link>
-          <Box display="flex">
-            <Box ml={3} fontWeight="fontWeightMedium">
-              <Link href="/landing-page">
-                Landing Page
-              </Link>
-            </Box>
-            <Box ml={3} fontWeight="fontWeightMedium">
-              <Link href="/ssr">
-                Server-side Rendering
-              </Link>
-            </Box>
-            <Box ml={3} fontWeight="fontWeightMedium">
-              <Link href="/static-generation">
-                Static Generation
-              </Link>
-            </Box>
-            <Box ml={3} fontWeight="fontWeightMedium">
-              <Link href="/spa">
-                Single Page App
-              </Link>
+const useStyles = makeStyles({
+  logo: {
+    '& svg': {
+      width: '120px',
+    },
+  },
+});
+
+const Layout = ({ children }: LayoutProps) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar color="inherit">
+        <Toolbar>
+          <Box
+            display="flex"
+            flex="1 1 100%"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Link href="/">
+              <Box className={classes.logo}>
+                <Logo />
+              </Box>
+            </Link>
+            <Box display="flex">
+              <Box ml={3} fontWeight="fontWeightMedium">
+                <Link href="/landing-page">
+                  Landing Page
+                </Link>
+              </Box>
+              <Box ml={3} fontWeight="fontWeightMedium">
+                <Link href="/ssr">
+                  Server-side Rendering
+                </Link>
+              </Box>
+              <Box ml={3} fontWeight="fontWeightMedium">
+                <Link href="/static-generation">
+                  Static Generation
+                </Link>
+              </Box>
+              <Box ml={3} fontWeight="fontWeightMedium">
+                <Link href="/spa">
+                  Single Page App
+                </Link>
+              </Box>
             </Box>
           </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container maxWidth={false}>
+        <Box my={4}>
+          {children}
         </Box>
-      </Toolbar>
-    </AppBar>
-    <Toolbar />
-    <Container maxWidth={false}>
-      <Box my={4}>
-        {children}
-      </Box>
-    </Container>
-  </>
-);
+      </Container>
+    </>
+  );
+};
 
 export default Layout;
